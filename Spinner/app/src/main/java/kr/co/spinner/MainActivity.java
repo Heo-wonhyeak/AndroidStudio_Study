75160ede27409afa09a988ede5dfbe15959a76ae
wonhyeakHeo <rkdcjf014777@gmail.com>
2022-10-03 12:24:35 +0900

    Spinner

---
+++ Spinner/app/src/main/java/kr/co/spinner/MainActivity.java
@@ -0,0 +1,45 @@
+package kr.co.spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        //리소스로부터 ArrayAdapter를 생성
+        ArrayAdapter<CharSequence> adapter =
+        ArrayAdapter.createFromResource(this,R.array.planets_array,android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //스피너와 어댑터 연결
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                Toast.makeText(MainActivity.this, "선택된 행성은"+adapterView.getItemAtPosition(pos).toString()
+                        , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+}
